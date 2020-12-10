d20b2c0e6ea89a120311757419ad1be62aab26cd
Peter Gibbons <gibbons.peter312@gmail.com>
2020-12-09 18:57:11 -0800

    tried fixing stuff and adding js
    spent hours trying to fix layouts and toggling my footer, but cant seem to be able to get it to work. compared resources on the web as well as past projects, feeling pretty bad

---
+++ Final-startingcode/apps.js
@@ -0,0 +1,9 @@
+const footer = document.querySelector(".footer")
+footer.classList.toggle("button");
+footer.classList.toggle("input");
+footer.classList.toggle("p");
+const id = document.getElementById("effect");
+
+
+
+
